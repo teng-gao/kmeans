5745c3e76bbbd8be95783cdb44a6af2a1b5816f2
Teng Gao <gaoteng@wustl.edu>
2017-12-11 00:37:12 -0600

    adding runtime chart

--- step3n4.sh
+++ step3n4.sh
@@ -47,3 +47,29 @@ hadoop fs -get dbpedia_cir_k6
 # visualize dbpedia data
 python2.7 visualize.py dbpedia_euc_k6/centers/part-00000 dbpedia_euc_k6/clusters/part-00000 comma kmeans/dbpedia_euc_k6.png
 python2.7 visualize.py dbpedia_cir_k6/centers/part-00000 dbpedia_cir_k6/clusters/part-00000 comma kmeans/dbpedia_cir_k6.png
+
+## compare runtime of three datasets using k = 4, 3 thread
+hadoop fs -rm -r synthetic_euc_k4_speed
+spark-submit --master local[3] kmeans_exec.py sample_geo.txt 4 Euclidean synthetic_euc_k4_speed/centers synthetic_euc_k4_speed/clusters tab
+# local-1512972580092 Start: 2017/12/11 00:09:34 End: 2017/12/11 00:10:08
+
+hadoop fs -rm -r device_euc_k4_speed
+spark-submit --master local[3] kmeans_exec.py device_data.txt 4 Euclidean device_euc_k4_speed/centers device_euc_k4_speed/clusters comma
+# local-1512973117213 Start: 2017/12/11 00:18:30 End: 2017/12/11 00:20:27
+
+hadoop fs -rm -r dbpedia_euc_k4_speed
+spark-submit --master local[3] kmeans_exec.py lat_longs.txt 4 Euclidean dbpedia_euc_k4_speed/centers dbpedia_euc_k4_speed/clusters space
+# local-1512972922310 Start: 2017/12/11 00:15:15 End: 2017/12/11 00:17:30
+
+# compare runtime of persist vs not persist
+hadoop fs -rm -r synthetic_euc_k4_speed_2
+spark-submit --master local[3] kmeans_exec_nopersist.py sample_geo.txt 4 Euclidean synthetic_euc_k4_speed_2/centers synthetic_euc_k4_speed_2/clusters tab
+# local-1512973329079 Start: 2017/12/11 00:22:02 End: 2017/12/11 00:22:30
+
+hadoop fs -rm -r device_euc_k4_speed_2
+spark-submit --master local[3] kmeans_exec_nopersist.py device_data.txt 4 Euclidean device_euc_k4_speed_2/centers device_euc_k4_speed_2/clusters comma
+# local-1512973361493 Start: 2017/12/11 00:22:35 End: 2017/12/11 00:24:46
+
+hadoop fs -rm -r dbpedia_euc_k4_speed_2
+spark-submit --master local[3] kmeans_exec_nopersist.py lat_longs.txt 4 Euclidean dbpedia_euc_k4_speed_2/centers dbpedia_euc_k4_speed_2/clusters space
+# local-1512973498778 Start: 2017/12/11 00:24:51 End: 2017/12/11 00:30:09
