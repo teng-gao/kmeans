dd452344283f97e39b845dc7fd7caf6dc4d01303
Teng Gao <gaoteng@wustl.edu>
2017-12-11 04:06:38 -0600

    reogranized

---
+++ scripts/kmeans_exec.py
@@ -0,0 +1,126 @@
+#Citation for formula used: https://superuser.com/questions/602798/how-to-do-a-great-circle-calculation-in-ms-excel-or-libreoffice
+import math, sys
+from operator import add
+from pyspark import SparkContext
+
+# Usage: kmeans_exec.py <input file> <k> <distance measure> <final centers output> <final cluster output> <delimiter>
+# hadoop fs -rm -r kmeans_out
+# spark-submit --master yarn-cluster kmeans_exec.py lat_longs_test.txt 4 Euclidean
+# spark-submit --master yarn-client kmeans_exec.py lat_longs_test.txt 4 Euclidean
+# spark-submit --master local[*] kmeans_exec.py lat_longs_test.txt 4 Euclidean
+# spark-submit --master yarn-cluster kmeans_exec.py lat_longs_test.txt 4 Euclidean
+# spark-submit --master yarn-client kmeans_exec.py lat_longs_test.txt 4 Euclidean
+# spark-submit --master local[*] kmeans_exec.py lat_longs.txt 4 Euclidean
+# spark-submit --master yarn-cluster kmeans_exec.py lat_longs.txt 4 GreatCircleDistance
+# hadoop fs -cat kmeans_out/final_centers/part-00000
+
+if __name__ == "__main__":
+    sc = SparkContext()
+
+    input_file = sys.argv[1]
+    k = int(sys.argv[2])
+    distance_measure = str(sys.argv[3])
+    final_centers_output = str(sys.argv[4])
+    final_clusters_output = str(sys.argv[5])
+    # set delimiter
+    if str(sys.argv[6]).lower() == 'tab':
+        sep = '\t'
+    elif str(sys.argv[6]).lower() == 'comma':
+        sep = ','
+    else:
+        sep = ' '
+
+    print("Reading from %s, k value %s" % (input_file, k))
+
+    def addPoints(p1,p2):
+        return map(add, p1, p2)
+
+    def EuclideanDistance(p1, p2):
+        return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+
+    def closestPoint(p, centers):
+        distances = [distance(p, center) for center in centers]
+        return distances.index(min(distances))
+
+    def GreatCircleDistance(p1, p2):
+        lat1 = p1[0]
+        longe1 = p1[1]
+        lat2 = p2[0]
+        longe2 = p2[1]
+    	radius = 3959 #miles
+    	lat1_radians = math.radians(lat1)
+    	longe1_radians = math.radians(longe1)
+    	lat2_radians = math.radians(lat2)
+    	longe2_radians = math.radians(longe2)
+    	dlat = lat2_radians - lat1_radians
+    	dlonge = longe2_radians - longe1_radians
+    	a = math.sin(dlat/2) * math.sin(dlat/2) + math.sin(dlonge/2) * math.sin(dlonge/2) * math.cos(lat1_radians) * math.cos(lat2_radians)
+    	c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    	d = radius * c
+    	return d
+
+    def distance(p1, p2):
+        return EuclideanDistance(p1,p2) if distance_measure == 'Euclidean' else GreatCircleDistance(p1, p2)
+
+    # read data and parse
+    dat = sc.textFile(input_file).map(lambda line: line.split(sep)).filter(lambda x: x[0] != "")
+
+    # augment the points by 1 to keep track of number of points summed
+    points = dat.map(lambda p: (float(p[0]), float(p[1]), 1))
+    points.persist()
+
+    # random generate initial k centers
+    centers = [(point[0], point[1]) for point in points.takeSample(True, k, seed = 0)]
+    print("Initial centers:")
+    print(centers)
+    new_centers = [(0,0)] * k
+    error = 0
+    errors = []
+    convergeDist = 0.1
+    change = 1
+    iteration = 1
+
+    print("Starting training..")
+    # iterate until converge
+    while change > convergeDist:
+        print("Iteration: %s" % iteration)
+        # produce (center index, augmented point) pair
+        center_point = points.keyBy(lambda point: closestPoint(point[:2], centers))
+        # compute the error
+        error = center_point.map(lambda (ci, p): distance(centers[ci], p)**2).reduce(lambda v1, v2: v1+v2)
+        errors.append(error)
+        print("Error of current center: %s" % error)
+        # sum all point coordinate for each cluster
+        sums = center_point.reduceByKey(addPoints).collect()
+        # get new center by taking avg of coordinate sums
+        new_centers = [c[1:] for c in sorted([(s[0], float(s[1][0])/s[1][2], float(s[1][1])/s[1][2]) for s in sums], key = lambda center: center[0])]
+        print("New centers: ")
+        print(new_centers)
+        # see if change of centers are small enough for termination
+        change = sum([distance(old, new) for (old, new) in zip(centers, new_centers)])
+        print("Change since last center: %s" % change)
+        # update the centers
+        centers = list(new_centers)
+        iteration += 1
+
+    # output final centers
+    centers_final = centers
+    print("Final centers: ")
+    print(centers_final)
+    print("Errors: ")
+    print(errors)
+
+    print("Saving final centers")
+    # sc.parallelize(centers_final).saveAsTextFile('kmeans_out/final_centers')
+    sc.parallelize(centers_final).saveAsTextFile(final_centers_output)
+
+    # final clusters
+    print("Saving final clusters")
+    final_clusters = points.keyBy(lambda point: closestPoint(point[:2], centers_final))
+
+    def RDD_to_lines(dat):
+        return ','.join([str(dat[1][0]), str(dat[1][1]), str(dat[0])])
+
+    lines = final_clusters.map(RDD_to_lines)
+    # lines.saveAsTextFile('kmeans_out/final_clusters')
+    lines.saveAsTextFile(final_clusters_output)
---
+++ scripts/step3n4.sh
@@ -0,0 +1,69 @@
+## Step 3
+# cluster synthetic location data sample_geo.txt
+hadoop fs -rm -r synthetic_euc_k4
+spark-submit --master local[*] kmeans_exec.py sample_geo.txt 4 Euclidean synthetic_euc_k4/centers synthetic_euc_k4/clusters tab
+hadoop fs -get synthetic_euc_k4
+
+hadoop fs -rm -r synthetic_euc_k2
+spark-submit --master local[*] kmeans_exec.py sample_geo.txt 2 Euclidean synthetic_euc_k2/centers synthetic_euc_k2/clusters tab
+rm -rf synthetic_euc_k2; hadoop fs -get synthetic_euc_k2
+
+hadoop fs -rm -r synthetic_cir_k4
+spark-submit --master local[*] kmeans_exec.py sample_geo.txt 4 GreatCircleDistance synthetic_cir_k4/centers synthetic_cir_k4/clusters tab
+hadoop fs -get synthetic_cir_k4
+
+hadoop fs -rm -r synthetic_cir_k2
+spark-submit --master local[*] kmeans_exec.py sample_geo.txt 2 GreatCircleDistance synthetic_cir_k2/centers synthetic_cir_k2/clusters tab
+hadoop fs -get synthetic_cir_k2
+
+# visualize synthetic location data
+python2.7 visualize.py synthetic_euc_k4/centers/part-00000 synthetic_euc_k4/clusters/part-00000 comma kmeans/trunk/synthetic_euc_k4.png
+python2.7 visualize.py synthetic_euc_k2/centers/part-00000 synthetic_euc_k2/clusters/part-00000 comma kmeans/trunk/synthetic_euc_k2.png
+python2.7 visualize.py synthetic_cir_k4/centers/part-00000 synthetic_cir_k4/clusters/part-00000 comma kmeans/trunk/synthetic_cir_k4.png
+python2.7 visualize.py synthetic_cir_k2/centers/part-00000 synthetic_cir_k2/clusters/part-00000 comma kmeans/trunk/synthetic_cir_k2.png
+
+# cluster device data
+hadoop fs -rm -r device_euc_k5
+spark-submit --master local[*] kmeans_exec.py device_data.txt 5 Euclidean device_euc_k5/centers device_euc_k5/clusters comma
+rm -rf device_euc_k5; hadoop fs -get device_euc_k5
+
+hadoop fs -rm -r device_cir_k5
+spark-submit --master local[*] kmeans_exec.py device_data.txt 5 GreatCircleDistance device_cir_k5/centers device_cir_k5/clusters comma
+rm -rf device_cir_k5; hadoop fs -get device_cir_k5
+
+# visualize device data
+python2.7 visualize.py device_euc_k5/centers/part-00000 device_euc_k5/clusters/part-00000 comma kmeans/trunk/device_euc_k5.png
+python2.7 visualize.py device_cir_k5/centers/part-00000 device_cir_k5/clusters/part-00000 comma kmeans/trunk/device_cir_k5.png
+
+# cluster DBpedia data
+hadoop fs -rm -r dbpedia_euc_k6
+spark-submit --master local[*] kmeans_exec.py lat_longs.txt 6 Euclidean dbpedia_euc_k6/centers dbpedia_euc_k6/clusters space
+hadoop fs -get dbpedia_euc_k6
+
+hadoop fs -rm -r dbpedia_cir_k6
+spark-submit --master local[*] kmeans_exec.py lat_longs.txt 6 GreatCircleDistance dbpedia_cir_k6/centers dbpedia_cir_k6/clusters space
+hadoop fs -get dbpedia_cir_k6
+
+# visualize dbpedia data
+python2.7 visualize.py dbpedia_euc_k6/centers/part-00000 dbpedia_euc_k6/clusters/part-00000 comma kmeans/trunk/dbpedia_euc_k6.png
+python2.7 visualize.py dbpedia_cir_k6/centers/part-00000 dbpedia_cir_k6/clusters/part-00000 comma kmeans/trunk/dbpedia_cir_k6.png
+
+## compare runtime of three datasets using k = 3, 3 thread
+for i in 1 2 3; do hadoop fs -rm -r synthetic_euc_k3_speed; spark-submit --master local[3] kmeans_exec.py sample_geo.txt 3 Euclidean synthetic_euc_k3_speed/centers synthetic_euc_k3_speed/clusters tab; done
+# local-1512972580092 Start: 2017/12/11 00:09:34 End: 2017/12/11 00:10:08
+
+for i in 1 2 3; do hadoop fs -rm -r device_euc_k3_speed; spark-submit --master local[3] kmeans_exec.py device_data.txt 3 Euclidean device_euc_k3_speed/centers device_euc_k3_speed/clusters comma; done
+# local-1512973117213 Start: 2017/12/11 00:18:30 End: 2017/12/11 00:20:27
+
+for i in 1 2 3; do hadoop fs -rm -r dbpedia_euc_k3_speed; spark-submit --master local[3] kmeans_exec.py lat_longs.txt 3 Euclidean dbpedia_euc_k3_speed/centers dbpedia_euc_k3_speed/clusters space; done
+# local-1512972922310 Start: 2017/12/11 00:15:15 End: 2017/12/11 00:17:30
+
+# compare runtime of persist vs not persist
+for i in 1 2 3; do hadoop fs -rm -r synthetic_euc_k3_speed_2; spark-submit --master local[3] kmeans_exec_nopersist.py sample_geo.txt 3 Euclidean synthetic_euc_k3_speed_2/centers synthetic_euc_k3_speed_2/clusters tab; done
+# local-1512973329079 Start: 2017/12/11 00:22:02 End: 2017/12/11 00:22:30
+
+for i in 1 2 3; do hadoop fs -rm -r device_euc_k3_speed_2; spark-submit --master local[3] kmeans_exec_nopersist.py device_data.txt 3 Euclidean device_euc_k3_speed_2/centers device_euc_k3_speed_2/clusters comma; done
+# local-1512973361493 Start: 2017/12/11 00:22:35 End: 2017/12/11 00:24:46
+
+for i in 1 2 3; do hadoop fs -rm -r dbpedia_euc_k3_speed_2; spark-submit --master local[3] kmeans_exec_nopersist.py lat_longs.txt 3 Euclidean dbpedia_euc_k3_speed_2/centers dbpedia_euc_k3_speed_2/clusters space; done
+# local-1512973498778 Start: 2017/12/11 00:24:51 End: 2017/12/11 00:30:09
